b1e010abfe0fac3d71a762d2f42a1efa62b76b23
Karol Tarasiuk <karol.tarasiuk@learnosity.com>
2014-10-01 15:28:38 +1000

    [LRN] Fixing x-browser issues with overline and overarrow symbols

--- test/unit/latex.test.js
+++ test/unit/latex.test.js
@@ -203,6 +203,9 @@ suite("latex", function() {
     assertParsesLatex('\\dottedsquare ');
     assertParsesLatex('\\square ');
     assertParsesLatex('\\rectangle ');
+    assertParsesLatex('\\overline{xyz}');
+    assertParsesLatex('\\overleftarrow{123}');
+    assertParsesLatex('\\overrightarrow{abc}');
   });
 
   suite('public API', function() {
